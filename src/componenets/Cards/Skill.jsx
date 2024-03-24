0159617a2f8b3c6c3b5d6c351a0c0545fef85e72
Ayush Singhal <ayushsinghal998@gmail.com>
2024-03-24 20:11:11 +0530

    fix skills hieght

--- src/componenets/Cards/Skill.jsx
+++ src/componenets/Cards/Skill.jsx
@@ -1,7 +1,7 @@
 export default function Skill({ data }) {
   return (
-    <div className="bg-surface-tertiary font-zodiak space-x-5 rounded-xl p-4 m-2 flex items-center sm:w-48 w-40">
-      <img src={data.imgSrc} alt={data.skill} className="sm:w-10 w-6" />
+    <div className="bg-surface-tertiary font-zodiak space-x-5 rounded-xl p-4 m-2 flex items-center sm:w-48 w-40 lg:h-20">
+      <img src={data.imgSrc} alt={data.skill} className="sm:w-10 w-7" />
       <span className="text-t-primary sm:text-lg text-base">{data.skill}</span>
     </div>
   );
