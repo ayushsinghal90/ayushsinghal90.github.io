c3959a486471697f192b7dac149e43e80925db88
Ayush Singhal <ayushsinghal998@gmail.com>
2024-03-24 20:29:45 +0530

    fit skills width

--- src/componenets/Cards/Skill.jsx
+++ src/componenets/Cards/Skill.jsx
@@ -1,6 +1,6 @@
 export default function Skill({ data }) {
   return (
-    <div className="bg-surface-tertiary font-zodiak space-x-5 rounded-xl p-4 m-2 flex items-center sm:w-48 w-40 lg:h-20">
+    <div className="bg-surface-tertiary font-zodiak space-x-5 rounded-xl p-4 m-2 flex items-center sm:w-48 w-44 lg:h-20">
       <img src={data.imgSrc} alt={data.skill} className="sm:w-10 w-7" />
       <span className="text-t-primary sm:text-lg text-base">{data.skill}</span>
     </div>
